ebc1c7ff60e02790dc9d437fb7b6c0dae468810d
ikemura23 <banbara.work@gmail.com>
2022-07-28 08:06:44 +0900

    defaultsコマンドを追加

--- install.sh
+++ install.sh
@@ -1,3 +1,5 @@
+#!/bin/sh
+
 # HomeBrewのインストール
 if [ ! -x "`which brew`" ]; then
   /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
@@ -51,3 +53,38 @@ brew cleanup
 
 ln -sf ~/dotfiles/.zshrc ~/.zshrc
 ln -sf ~/dotfiles/.gitconfig ~/.gitconfig
+
+# --- Finder ---
+# 隠しファイルを表示
+defaults write com.apple.finder AppleShowAllFiles -bool true
+# すべての拡張子のファイルを表示
+defaults write NSGlobalDomain AppleShowAllExtensions -bool true
+# ステータスバーを表示
+defaults write com.apple.finder ShowStatusBar -bool true
+# パスバーを表示
+defaults write com.apple.finder ShowPathbar -bool true
+# ドライブをデスクトップに表示
+killall Finder
+
+# ネットワークストレージに .DS_Store ファイルを作成しない
+defaults write com.apple.desktopservices DSDontWriteNetworkStores -bool true
+# USBメモリに .DS_Store ファイルを作成しない
+defaults write com.apple.desktopservices DSDontWriteUSBStores -bool true
+
+# --- Dock ---
+# “自動的に非表示”をオン
+defaults write com.apple.dock autohide -bool true
+# 最近使ったアプリケーションを非表示
+defaults write com.apple.dock show-recents -bool false
+killall Dock
+
+# --- SystemUIServer関係 ---
+# 時計で日付を表示（例：9月20日(木) 23:00）
+defaults write com.apple.menuextra.clock DateFormat -string 'EEE MMM d HH:mm:ss'
+# バッテリーの割合（%）を表示
+defaults write com.apple.menuextra.battery ShowPercent -string "YES"
+# スクリーンショットのドロップシャドウを付けない
+defaults write com.apple.screencapture disable-shadow -bool true
+killall SystemUIServer
+
+echo "install done"
